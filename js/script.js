2f909c6f950f85766a78027446d48f5720077662
Allikk38 <alexanderfobia@yandex.ru>
2026-05-19 17:57:50 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // ===== КОНФИГУРАЦИЯ =====
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzZT7dsLRkNLE0Lp7zzi1oWfGhqdxFhydKV_B-B6GZMBHp7r6OND6LgduyP89YclZQo/exec'; 
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyHh9o63J7uFn6KaiYyDgCSee4L_3tV1NH6iqox7-QtTsx-B-uYapu-wUrtwWyBPxgI/exec'; 
 const MAP_CENTER = [55.018, 82.92];
 const MAP_ZOOM = 12;
 
