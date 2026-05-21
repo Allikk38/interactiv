ee0aab7857175d805dd991e143255b3a497dedfb
Allikk38 <alexanderfobia@yandex.ru>
2026-05-21 13:40:59 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // ===== КОНФИГУРАЦИЯ =====
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyHh9o63J7uFn6KaiYyDgCSee4L_3tV1NH6iqox7-QtTsx-B-uYapu-wUrtwWyBPxgI/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxAqmEy0aOTu-MTIeP_A0lf684Jdowk5N0IYss8jBJBzjibrNz80xeuaYKv0QCXYrVP/exec';
 const MAP_CENTER = [55.018, 82.92];
 const MAP_ZOOM = 12;
