518841a53a32e5ca043e7846df16abab7cc0b5e9
Allikk38 <alexanderfobia@yandex.ru>
2026-05-28 11:53:15 +0700

    Update escape.js

--- js/utils/escape.js
+++ js/utils/escape.js
@@ -4,7 +4,7 @@
 /**
  * Экранирует HTML-спецсимволы в строке
  * @param {string} str - входная строка
- *returns {string} экранированная строка
+ * @returns {string} экранированная строка
  */
 function escapeHtml(str) {
     if (!str) return '';
@@ -15,7 +15,3 @@ function escapeHtml(str) {
         .replace(/"/g, '&quot;')
         .replace(/'/g, '&#39;');
 }
-
-// Для обратной совместимости с map-core.js
-// (можно будет заменить прямые вызовы позже)
-window.escapeHtml = escapeHtml;
